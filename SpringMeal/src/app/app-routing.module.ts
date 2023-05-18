91f442fe75b84f365f14352772b3f26c12fe665f
Palmira Romia <kguinera@hotmail.com>
2023-05-18 09:47:00 +0200

    icon logo and some changes

--- SpringMeal/src/app/app-routing.module.ts
+++ SpringMeal/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
+
+
+import { LoginComponent } from './Login/login/login.component';
+import { RegisterComponent } from './Login/register/register.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'login' },
