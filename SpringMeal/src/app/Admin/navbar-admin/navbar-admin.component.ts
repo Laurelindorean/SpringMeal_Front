d9aca6777280a281d38ed80f3c152e8fa45005b1
Palmira Romia <kguinera@hotmail.com>
2023-05-27 20:25:08 +0200

    logout button on admin navbar

--- SpringMeal/src/app/Admin/navbar-admin/navbar-admin.component.ts
+++ SpringMeal/src/app/Admin/navbar-admin/navbar-admin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 import { MenuAdminService } from 'src/app/Service/menu-admin.service';
 import { MenuAdmin } from 'src/app/interfaces/MenuAdmin';
 
@@ -11,7 +12,7 @@ import { MenuAdmin } from 'src/app/interfaces/MenuAdmin';
 export class NavbarAdminComponent implements OnInit {
   menu: MenuAdmin[] = [];
 
-  constructor(private menuService: MenuAdminService, private router: Router) {}
+  constructor(private menuService: MenuAdminService, private router: Router, private cookie:CookieService) {}
 
   ngOnInit(): void {
     this.getMenu();
@@ -24,5 +25,9 @@ export class NavbarAdminComponent implements OnInit {
   }
   back() {
     this.router.navigateByUrl('/welcome');
+  }
+  logOut(){
+    this.cookie.deleteAll("/");
+    this.router.navigateByUrl('/login');
   }
 }
